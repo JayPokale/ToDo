9a5e6cf757bf799546582f0956b55bb57ac0aa68
JayPokale <jay.pokale.35@gmail.com>
2022-12-25 11:13:26 +0530

    Final Changes

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
-import path from "path"
+import path from "path";
 dotenv.config();
 import todo from "./routes/todo.js";
 
@@ -21,17 +21,13 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api/todo", todo);
-app.use(express.static(".next/server/pages"))
-app.get("*", (req, res) => {
-  res.render(path.dirname + "/.next/server/pages/index.html");
-});
 
 mongoose.connect(process.env.MONGO_URL, (err) => {
   err ? console.log(err.message) : console.log("connected");
-  err ||
-    app.listen(port, (error) => {
-      error
-        ? console.log(error.message)
-        : console.log("Server is running on:", port);
-    });
+});
+
+app.listen(port, (error) => {
+  error
+    ? console.log(error.message)
+    : console.log("Server is running on:", port);
 });
